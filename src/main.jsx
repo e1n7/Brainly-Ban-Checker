0ffeb4646e048544ff390acd387455486e21aebc
Sefton <sef81473@gmail.com>
2026-05-12 20:13:38 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom' // You might need to install this
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter basename="/Brainly-Ban-Checker">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
